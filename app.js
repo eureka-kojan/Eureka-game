eb59f33a6417f12add7e548ad0b97464703e7ae7
gustuj <jhs_0622@naver.com>
2021-10-24 00:29:22 +0900

    Add simple test code

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main(){
+    await mongoose.connect('mongodb://localhost:27017/test')
+}
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+    class_num: String,
+    point: Number
+})
+
+const Student = mongoose.model('Student', studentSchema);
+
+const test_student = new Student({name:'제현승', class_num:'20703', point:100});
+console.log(silence.name);
